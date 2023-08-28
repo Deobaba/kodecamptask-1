07b084e1ebca088062b6e20d9a5b581d011e2dc6
Adenekan Adeola <104663055+Deobaba@users.noreply.github.com>
2023-08-28 14:31:58 +0100

    add few things

--- library.js
+++ library.js
@@ -18,7 +18,7 @@ function Book(title, author, genre) {
   };
   
   // Generator function for bookIterator
-  LibraryCatalog.prototype.bookIterator = function* () {
+  LibraryCatalog.prototype.bookIterator = function*() {
     for (const book of this.books) {
       yield book;
     }
@@ -29,17 +29,24 @@ function Book(title, author, genre) {
     return this.bookIterator();
   };
   
-  // Get books by a specific author
+  // Get books by a author
   LibraryCatalog.prototype.getBooksByAuthor = function(authorName) {
     return this.books.filter(book => book.author === authorName);
   };
+
+  LibraryCatalog.prototype.getBooksByTitle = function(titleName) {
+    return this.books.filter(book => book.title === titleName);
+  };
   
+  LibraryCatalog.prototype.getBooksByGenre = function(genreName) {
+    return this.books.filter(book => book.title === genreName);
+  };
  
   const library = new LibraryCatalog();
-  library.addBook('Book 1', 'Author A', 'Fantasy');
-  library.addBook('Book 2', 'Author B', 'Mystery');
-  library.addBook('Book 3', 'Author A', 'Science Fiction');
-  
+  library.addBook('things fall apart', 'chinua Achebe', 'Epic');
+  library.addBook('Independence', 'Wole Soyinka', 'Mystery');
+  library.addBook('48 laws of power', 'Robert Greene', 'Reality');
+  library.addBook('Art of Seduction', 'Robert Greene', 'Reality')
   // Iterate over books using for...of loop
   console.log('All books in the catalog:',library);
   for (const book of library) {
@@ -47,7 +54,7 @@ function Book(title, author, genre) {
   }
   
   // Get books by a specific author
-  const authorName = 'Author A';
+  const authorName = 'Robert Greene';
   const booksByAuthor = library.getBooksByAuthor(authorName);
   console.log(`Books by ${authorName}:`, booksByAuthor);
   
